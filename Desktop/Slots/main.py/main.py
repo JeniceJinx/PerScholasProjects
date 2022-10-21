fcb5ff35955b53ad405e67377438f26d932e45f3
Jinx <JeniceJinx@users.noreply.github.com>
2022-10-21 19:45:55 -0400

    first commit

---
+++ Desktop/Slots/main.py/main.py
@@ -0,0 +1,8 @@
+from curses.ascii import isdigit
+
+
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
